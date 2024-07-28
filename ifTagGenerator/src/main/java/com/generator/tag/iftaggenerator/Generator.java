736c71f5cdd26c6645d09f68504635d8bf184a93
ggabdoll <ggabdoll0327@naver.com>
2024-07-28 23:45:13 +0900

    아직 잘 모르겠음

--- ifTagGenerator/src/main/java/com/generator/tag/iftaggenerator/Generator.java
+++ ifTagGenerator/src/main/java/com/generator/tag/iftaggenerator/Generator.java
@@ -12,7 +12,6 @@ import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiParameter;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiShortNamesCache;
-import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,27 +31,26 @@ public class Generator extends AnAction {
     }
 
     PsiElement element = e.getData(CommonDataKeys.PSI_ELEMENT);
-    if (!(element instanceof XmlAttributeValue)) {
+    if (!(element instanceof XmlTag)) {
       return;
     }
 
-    XmlAttributeValue attributeValue = (XmlAttributeValue) element;
-    XmlTag xmlTag = (XmlTag) attributeValue.getParent().getParent();
+    XmlTag xmlTag = (XmlTag) element;
 
-    System.out.println(xmlTag.getName());
+    System.out.println("XML TAG >>>>> " + xmlTag.getName());
 
-    if (isMyBatisTag(xmlTag.getName())) {
-      String interfaceName = xmlTag.getAttributeValue(
-          "interface"); // 인터페이스 이름이 "interface" 속성에 있다고 가정합니다.
-      if (interfaceName != null) {
-        PsiClass[] classes = PsiShortNamesCache.getInstance(project)
-            .getClassesByName(interfaceName, GlobalSearchScope.allScope(project));
-        if (classes.length > 0) {
-          PsiClass psiClass = classes[0];
-          showMethodParameters(psiClass);
-        }
+    String namespace = xmlTag.getParentTag().getAttributeValue("namespace");
+    String methodId = xmlTag.getAttributeValue("id");
+
+    if (namespace != null && methodId != null) {
+      PsiClass[] classes = PsiShortNamesCache.getInstance(project)
+          .getClassesByName(namespace, GlobalSearchScope.allScope(project));
+      if (classes.length > 0) {
+        PsiClass psiClass = classes[0];
+        showMethodParameters(psiClass, methodId);
       }
     }
+
 
   }
 
@@ -61,20 +59,21 @@ public class Generator extends AnAction {
         "delete".equals(tagName) || "select".equals(tagName) || "sql".equals(tagName);
   }
 
-  private void showMethodParameters(PsiClass psiClass) {
-    StringBuilder parametersInfo = new StringBuilder("Methods and their parameters:\n");
+  private void showMethodParameters(PsiClass psiClass, String methodId) {
     for (PsiMethod method : psiClass.getMethods()) {
-      parametersInfo.append(method.getName()).append("(");
-      for (PsiParameter parameter : method.getParameterList().getParameters()) {
-        parametersInfo.append(parameter.getType().getPresentableText()).append(" ")
-            .append(parameter.getName()).append(", ");
+      if (method.getName().equals(methodId)) {
+        StringBuilder parametersInfo = new StringBuilder(
+            "Parameters for method " + methodId + ":\n");
+        for (PsiParameter parameter : method.getParameterList().getParameters()) {
+          parametersInfo.append(parameter.getType().getPresentableText()).append(" ")
+              .append(parameter.getName()).append("\n");
+        }
+        Messages.showMessageDialog(parametersInfo.toString(), "Method Parameters",
+            Messages.getInformationIcon());
+        return;
       }
-      if (method.getParameterList().getParametersCount() > 0) {
-        parametersInfo.setLength(parametersInfo.length() - 2); // 마지막 쉼표와 공백 제거
-      }
-      parametersInfo.append(")\n");
     }
-    Messages.showMessageDialog(parametersInfo.toString(), "Method Parameters",
-        Messages.getInformationIcon());
+    Messages.showMessageDialog("No method found with id " + methodId, "Error",
+        Messages.getErrorIcon());
   }
 }
